a309223ceb2bda20b8a284fc1a1a0614db6a86a7
juanerq <jrjuanreyes64@gmail.com>
2024-02-14 19:50:38 -0500

    ⚙️ Configuraciones adicionals de swagger, modulos, eventos, filtros globales y prefijo de api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { validateEnvironment } from './config/env/env.validation';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmConfigService } from './config/database/typeorm.config';
+import { RolesModule } from './contexts/roles/roles.module';
+import { AuthModule } from './contexts/auth/auth.module';
+import { ModulesModule } from './contexts/modules/modules.module';
+import { PermissionsModule } from './contexts/permissions/permissions.module';
+import { EmailModule } from './contexts/email/email.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
@@ -14,6 +20,12 @@ import { TypeOrmConfigService } from './config/database/typeorm.config';
     TypeOrmModule.forRootAsync({
       useClass: TypeOrmConfigService,
     }),
+    RolesModule,
+    AuthModule,
+    ModulesModule,
+    PermissionsModule,
+    EmailModule,
+    EventEmitterModule.forRoot(),
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { QueryExceptionFilter } from './common/filters/query-exception.filter';
+import { swaggerConfig } from './config/swagger/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -12,8 +16,15 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  app.setGlobalPrefix('api');
+
+  app.useGlobalFilters(new HttpExceptionFilter(), new QueryExceptionFilter());
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  swaggerConfig(app);
 
   await app.listen(port);
 }
