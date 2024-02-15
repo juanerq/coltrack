8eb8550fa9836645a60e4f1cc92d950e9f949ce2
juanerq <jrjuanreyes64@gmail.com>
2024-02-14 19:52:19 -0500

    📨 Creación de servicio de envio de correos por eventos y personalidación de plantillas para la validación de correo y bienvenida

---
+++ src/contexts/email/email.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('MAIL_HOST'),
+          port: 465,
+          secure: true,
+          auth: {
+            user: configService.get('SMTP_EMAIL'),
+            pass: configService.get('SMTP_SECRET_KEY'),
+          },
+        },
+        defaults: {
+          from: `"From Coltrack" <${configService.get('SMTP_EMAIL')}>`,
+        },
+        template: {
+          dir: join(__dirname, 'templates'),
+          adapter: new EjsAdapter(),
+          options: {
+            strict: false,
+          },
+        },
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [EmailService],
+})
+export class EmailModule {}
